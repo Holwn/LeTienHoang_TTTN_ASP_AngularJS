1a9b3fe143e020517cb65642104651a67b3433fa
Holwn <ltienhoang2@gmail.com>
2023-04-17 23:08:26 +0700

    Add ErrorTable for Log Error Add PostCategoryTrontroller for api Update Pharma.Web Project

---
+++ Pharma.Data/Repositories/ErrorRepository.cs
@@ -0,0 +1,15 @@
+﻿using Pharma.Data.Infrastructure;
+using Pharma.Model.Models;
+
+namespace Pharma.Data.Repositories
+{
+    public interface IErrorRepository : IRepository<Error>
+    { }
+
+    public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
+    {
+        public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ Pharma.Web/Api/PostCategoryController.cs
@@ -0,0 +1,96 @@
+﻿using Pharma.Model.Models;
+using Pharma.Service;
+using Pharma.Web.Infrastructure.Core;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Pharma.Web.Api
+{
+    [RoutePrefix("api/postcategory")]
+    public class PostCategoryController : ApiControllerBase
+    {
+        IPostCategoryService _postCategoryService;
+        public PostCategoryController(IErrorService errorService,IPostCategoryService postCategoryService) : base(errorService)
+        {
+            this._postCategoryService = postCategoryService;
+        }
+        [Route("getall")]
+        public HttpResponseMessage Get(HttpRequestMessage requestMessage, PostCategory postCategory)
+        {
+            return CreateHttpReponse(requestMessage, () =>
+            {
+                HttpResponseMessage responseMessage = null;
+                if (ModelState.IsValid)
+                {
+                    requestMessage.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var listCategory = _postCategoryService.GetAll();
+
+                    responseMessage = requestMessage.CreateResponse(HttpStatusCode.OK, listCategory);
+                }
+                return responseMessage;
+            });
+        }
+        public HttpResponseMessage Post(HttpRequestMessage requestMessage,PostCategory postCategory)
+        {
+            return CreateHttpReponse(requestMessage, () =>
+             {
+                 HttpResponseMessage responseMessage = null;
+                 if(ModelState.IsValid)
+                 {
+                     requestMessage.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                 }
+                 else
+                 {
+                     var category = _postCategoryService.Add(postCategory);
+                     _postCategoryService.Save();
+
+                     responseMessage = requestMessage.CreateResponse(HttpStatusCode.Created, category);
+                 }
+                 return responseMessage;
+             });
+        }
+        public HttpResponseMessage Put(HttpRequestMessage requestMessage, PostCategory postCategory)
+        {
+            return CreateHttpReponse(requestMessage, () =>
+            {
+                HttpResponseMessage responseMessage = null;
+                if (ModelState.IsValid)
+                {
+                    requestMessage.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    _postCategoryService.Update(postCategory);
+                    _postCategoryService.Save();
+
+                    responseMessage = requestMessage.CreateResponse(HttpStatusCode.OK);
+                }
+                return responseMessage;
+            });
+        }
+        public HttpResponseMessage Delete(HttpRequestMessage requestMessage, int id)
+        {
+            return CreateHttpReponse(requestMessage, () =>
+            {
+                HttpResponseMessage responseMessage = null;
+                if (ModelState.IsValid)
+                {
+                    requestMessage.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    _postCategoryService.Delete(id);
+                    _postCategoryService.Save();
+
+                    responseMessage = requestMessage.CreateResponse(HttpStatusCode.OK);
+                }
+                return responseMessage;
+            });
+        }
+    }
+}
