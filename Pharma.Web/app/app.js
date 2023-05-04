88ccba81e2ec226a50c27f641c4357621cc42cc0
Holwn <ltienhoang2@gmail.com>
2023-05-05 01:29:02 +0700

    add asset library for admin set angularjs app

---
+++ Pharma.Web/app/app.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="../assets/admin/libs/angularjs/angular.js" />
+
+(function () {
+    angular.module('pharma', ['pharma.products', 'pharma.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
---
+++ Pharma.Web/app/shared/modules/pharma.common.js
@@ -0,0 +1,3 @@
+﻿(function () {
+    angular.module('pharma.common',['ui.router'])
+})();
